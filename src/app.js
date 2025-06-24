5001c000e4adb60f95d546535cc7772cfc0fe0f5
armmani <arm.maniii@gmail.com>
2025-06-24 13:41:42 +0700

    errorMiddleware done

--- src/app.js
+++ src/app.js
@@ -1,17 +1,21 @@
 import express from "express";
 import authRoute from "./routes/auth.route.js";
 import notFoundMiddleware from "./middlewares/not-found.middleware.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
 app.use("/api/post", (req, res) => {
+  console.log(xyz);
   res.json({ body: req.body });
 });
 app.use("/api/comment", (req, res) => res.send("comment Service"));
 app.use("/api/like", (req, res) => res.send("like Service"));
 
 app.use(notFoundMiddleware);
+
+app.use(errorMiddleware);
 
 export default app;
