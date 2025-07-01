afff99ad72639803b78cacc9b39bfa414d69992b
armmani <arm.maniii@gmail.com>
2025-07-01 15:06:40 +0700

    delete post

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -2,9 +2,24 @@ import fs from "fs/promises";
 import cloudinary from "../config/cloudinary.config.js";
 import path from "path";
 import prisma from "../config/prisma.config.js";
+import createErrorUtil from "../utils/create-error.util.js";
 
 export const getAllPosts = async (req, res, next) => {
-  res.json({ message: "Get All POSTs" });
+  const resp = await prisma.post.findMany({
+    orderBy: { createdAt: "desc" },
+    include: {
+      user: {
+        select: {
+          firstName: true,
+          lastName: true,
+          profileImage: true,
+        },
+      },
+      comments: true,
+      likes: true,
+    },
+  });
+  res.json({ posts: resp });
 };
 
 export const createPost = async (req, res, next) => {
@@ -23,7 +38,7 @@ export const createPost = async (req, res, next) => {
   }
   const data = {
     message: message,
-    image: uploadResult?.secure_url || '',
+    image: uploadResult?.secure_url || "",
     userId: req.user.id,
   };
   const rs = await prisma.post.create({ data });
@@ -36,5 +51,18 @@ export const updatePost = async (req, res, next) => {
 };
 
 export const deletePost = async (req, res, next) => {
-  res.json({ message: "DELETE Post" });
+  const { id } = req.params;
+  const foundPost = await prisma.post.findUnique({
+    where: { id: +id },
+  });
+  if (!foundPost) {
+    createErrorUtil(400, "POST ID NOT FOUND");
+  }
+  if (req.user.id !== foundPost.userId) {
+    createErrorUtil(400, "Unable to delete");
+  }
+  const rs = await prisma.post.delete({
+    where: { id: +id },
+  });
+  res.json({ message: "DELETE SUCCESSFUL" });
 };
