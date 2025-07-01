3c4de4d02d1f64766b16aa32b385d620116e52c0
armmani <arm.maniii@gmail.com>
2025-07-01 09:20:28 +0700

    PostItem done

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import notFoundMiddleware from "./middlewares/not-found.middleware.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import cors from "cors";
 import postRoute from "./routes/post.route.js";
+import authenticateMiddleware from "./middlewares/authenticate.middleware.js";
 
 const app = express();
 app.use(
@@ -14,7 +15,7 @@ app.use(
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
-app.use("/api/post", postRoute);
+app.use("/api/post",authenticateMiddleware, postRoute);
 app.use("/api/comment", (req, res) => res.send("comment Service"));
 app.use("/api/like", (req, res) => res.send("like Service"));
 
--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,11 +1,34 @@
+import fs from "fs/promises";
+import cloudinary from "../config/cloudinary.config.js";
+import path from "path";
+import prisma from "../config/prisma.config.js";
+
 export const getAllPosts = async (req, res, next) => {
   res.json({ message: "Get All POSTs" });
 };
 
 export const createPost = async (req, res, next) => {
-  console.log(req.body.message)
-  console.log(req.file)
-  res.json({ message: "Create Post", file: req.file });
+  const { message } = req.body;
+  console.log(req.file);
+  let haveFile = !!req.file;
+  let uploadResult = null;
+
+  console.log("cloudinary", cloudinary);
+  if (haveFile) {
+    uploadResult = await cloudinary.uploader.upload(req.file.path, {
+      overwrite: true,
+      public_id: path.parse(req.file.path).name,
+    });
+    fs.unlink(req.file.path);
+  }
+  const data = {
+    message: message,
+    image: uploadResult.secure_url,
+    userId: req.user.id,
+  };
+  const rs = await prisma.post.create({ data });
+
+  res.status(201).json({ message: "Create Post", result: rs });
 };
 
 export const updatePost = async (req, res, next) => {
